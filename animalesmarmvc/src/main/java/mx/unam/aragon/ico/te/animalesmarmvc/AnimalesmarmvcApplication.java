fad38d2ba3dcbb1239438381d36b9f75b5c985c1
AndreaAvi <andreaca@outlook.com>
2025-05-19 16:59:17 -0600

    Primer snapshot: proyecto creado

---
+++ animalesmarmvc/src/main/java/mx/unam/aragon/ico/te/animalesmarmvc/AnimalesmarmvcApplication.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.ico.te.animalesmarmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalesmarmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalesmarmvcApplication.class, args);
+	}
+
+}
---
+++ animalesmarmvc/src/test/java/mx/unam/aragon/ico/te/animalesmarmvc/AnimalesmarmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.ico.te.animalesmarmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalesmarmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
